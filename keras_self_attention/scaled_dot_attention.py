Fix key & value masking

keras_self_attention/scaled_dot_attention.py
@@ -55,7 +55,7 @@ class ScaledDotProductAttention(keras.layers.Layer):
             ones = tf.ones((query_len, key_len))
             e -= (ones - tf.matrix_band_part(ones, -1, 0)) * 1e9
         if isinstance(mask, list) and mask[-1] is not None:
-            e -= (1.0 - K.cast(K.expand_dims(mask[-1]), K.floatx())) * 1e9
+            e -= (1.0 - K.cast(K.expand_dims(mask[-1], axis=-2), K.floatx())) * 1e9
         a = keras.activations.softmax(e)
         v = K.batch_dot(a, value)
         if self.return_attention:

tests/scaled_dot_attention/test_sample.py
@@ -39,6 +39,6 @@ class TestAttention(unittest.TestCase):
         predict = model.predict(inputs)[0]
         self.assertTrue(np.allclose(predict[0], predict[3]))
         self.assertTrue(np.allclose(
-            np.asarray([0.24973875, 0.41574854, 0.44609764, 0.644485, 0.47796533]),
+            np.asarray([0.27883747, 0.45767492, 0.47448885, 0.69199574, 0.47368336]),
             predict[2],
         ), predict[2])