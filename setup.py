d65ab8db6fc88c5a6efe6ca5782e5877ba630cf6
CyberZHG <CyberZHG@gmail.com>
2019-01-31 09:04:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
-from setuptools import setup
+import codecs
+from setuptools import setup, find_packages
+
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
 
 setup(
     name='keras-self-attention',
-    version='0.33.0',
-    packages=['keras_self_attention'],
+    version='0.34.0',
+    packages=find_packages(),
     url='https://github.com/CyberZHG/keras-self-attention',
     license='MIT',
     author='CyberZHG',
     author_email='CyberZHG@gmail.com',
     description='Attention mechanism for processing sequential data that considers the context for each timestamp',
-    long_description=open('README.rst', 'r').read(),
-    install_requires=[
-        'numpy',
-        'keras',
-    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
     classifiers=(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
